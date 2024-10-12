cdd409129b3a48a91ec53376af50bb231c2b5a36
Divine <officialkb17@gmail.com>
2024-10-12 16:57:34 +0100

    write token contract

--- src/VineToken.sol
+++ src/VineToken.sol
@@ -1,4 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract VineToken {}
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract VineToken is ERC20, Ownable {
+    constructor() ERC20("Vine Token", "VINE") Ownable(msg.sender) { }
+
+    function mint(address account, uint256 amount) external onlyOwner {
+        _mint(account, amount);
+    }
+}
